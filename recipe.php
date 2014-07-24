45b58d8ce0a5d3f470129ba760b5e9c45aadd3e2
crisgorrino <cgorrino@gmail.com>
2014-07-24 16:50:29 -0500

    recipe page

--- recipe.php
+++ recipe.php
@@ -65,7 +65,88 @@
 	</ul>
 	
 </section>
+
+<section class="inner recipe-main cf">
+	
+	<div class="column left">
+		<h3>Ingeredients</h3>
+		<ul>
+			<li>2/3 c <strong>Stonyfield Organic Greek Plain Nonfat</strong></li> 
+			<li>1/4 c basil</li>
+			<li>2 T olive oil</li>
+			<li>3/4 t salt</li>
+			<li>1/4 T black pepper</li>
+			<li>1 T unsalted butter</li>
+			<li>1 small onion (finely chopped)</li>
+			<li>1 c Quinoa (rinsed)</li>
+			<li>1 1/4 c low-sodium vegetable broth</li>
+			<li>2 small zucchini (trimmed and grated)</li>
+			<li>1 red pepper (seeded and cut into 1/4-inch pieces)</li>
+			<li>1 cn chickpeas (drained and rinsed)</li>
+			<li>8 oz fresh mozzarella (cut into 1/2-inch pieces)</li>
+		</ul>
+	</div>
+	
+	<div class="column right">
+		<h3>Preparation</h3>
+		<h4>Step 1</h4>
+		<p>Combine yogurt, basil, olive oil, 1/2 teaspoon of the salt and the pepper in a blender. Puree on high speed until well blended and smooth; set aside.</p>
+		
+		<h4>Step 2</h4>
+		<p>In a medium-size saucepan, melt butter over medium heat. Add onion to pot and cook for 4 minutes. Add quinoa to pot and cook, stirring, for 1 minute. Stir in broth, cover and bring to a boil over high heat. Reduce heat to medium-low and simmer, covered, for about 20 minutes or until liquid is absorbed. Remove from heat and transfer quinoa to a serving bowl.</p>
+		
+		<h4>Step 3</h4>
+		<p>Stir prepared dressing, zucchini, red pepper, chickpeas, mozzarella and remaining 1/4 teaspoon salt into serving bowl with quinoa until combined; serve immediately.</p>
+	</div>
+	
+</section>
 <!--recipe content-->
+
+<!--other happy recepies-->
+<article class="container recent-recipe-cont">
+	<section class="inner center recent-recipes">
+		<img src="img/recent-recipies-left-short.jpg" alt="" class="inline"><h2 class="inline">Other Happy Recipes</h2><img src="img/recent-recipies-right-short.jpg" alt="" class="inline">
+	</section>
+	<section class="inner cf"> 
+		<div class="recent-recipe">
+			<img src="img/recent-recipe.jpg" alt="recent recipe">
+			
+			<div class="recent-recipe-info">
+				<p>Burgers Anyone</p>
+				<a href="" class="recipe-nums">
+					Recipes(7)
+				</a>
+			</div>
+			
+		</div>
+		
+		<div class="recent-recipe">
+			<img src="img/recent-recipe.jpg" alt="recent recipe">
+			
+			<div class="recent-recipe-info">
+				<p>Burgers Anyone</p>
+				<a href="" class="recipe-nums">
+					Recipes(7)
+				</a>
+			</div>
+			
+		</div>
+		
+		<div class="recent-recipe">
+			<img src="img/recent-recipe.jpg" alt="recent recipe">
+			
+			<div class="recent-recipe-info">
+				<p>Burgers Anyone</p>
+				<a href="" class="recipe-nums">
+					Recipes(7)
+				</a>
+			</div>
+			
+		</div>
+	</section>
+</article>
+<!--other happy recepies-->
+
 
 
 <!--include--footer-->
